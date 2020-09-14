08934247515e50dd0e7a60dd57f81f5a741ad90a
Milky <tpdhks0369@gmail.com>
2020-09-14 21:18:44 +0900

    feat : metaspace에 대한 내용

---
+++ java8to11/src/main/java/com/example/demo/metaspace/App.java
@@ -0,0 +1,13 @@
+package com.example.demo.metaspace;
+
+public class App {
+
+    // JVM 메모리상 PermGem 영역이 없어지고 Metaspace 가 생겼다. Native Memory로 이동했고 고정된 메모리가 없다.
+    // Native Memeory가 끝날때까지 증식함. 때문에 최대값을 설정해야 한다.
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
